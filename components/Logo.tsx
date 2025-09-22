6beac7e5cde818aa8df1d79e21d7e07bb6d918f0
Deepanshu Chauhan <deepanshuchauhan483@gmail.com>
2025-09-22 19:27:02 +0530

    added Sidebar and Breadcrumbs

---
+++ components/Logo.tsx
@@ -0,0 +1,24 @@
+import { cn } from '@/lib/utils';
+import { SquareDashedMousePointer } from 'lucide-react';
+import Link from 'next/link';
+import React from 'react'
+
+const Logo = ({fontSize="text-2xl", iconsSize=20}:{
+  fontSize?: string;
+  iconsSize?: number;
+}) => {
+  return (
+    <Link href="/" className={cn("text-2xl font-extrabold flex items-center gap-2",fontSize)}>
+      <div className="rounded-xl bg-gradient-to-r from-orange-400 to bg-orange-500 p-2">
+        <SquareDashedMousePointer size={iconsSize} className="stroke-white"/>
+      </div>
+      <div>
+        <span className="bg-gradient-to-r from-orange-400 to bg-orange-500 bg-clip-text text-transparent">Flow</span>
+        <span className='text-stone-700 dark:text-stone-300'>Scrape</span>
+      </div>
+
+    </Link>
+  )
+}
+
+export default Logo
